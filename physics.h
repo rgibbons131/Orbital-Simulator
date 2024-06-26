4eaedff9134570660050478d47a7671186332f57
rgibbons131 <richiegibbons99@gmail.com>
2024-06-26 09:12:09 -0600

    Add physics comments

--- physics.h
+++ physics.h
@@ -13,41 +13,97 @@ const float TPF = TD / FRAME_RATE;
 const float EARTH_RADIUS = 6378000;
 const float GRAVITY = 9.80665;
 
+/***************************************************************
+* getEarthRotationSpeed() 
+* Returns the speed at which the Earth should rotate
+******************************************************************/
  inline float getEarthRotationSpeed() 
 {
    return -(2.0 * M_PI / FRAME_RATE) * (TD / 86400.0);
 }
 
+ /***************************************************************
+* getGravity(float height)
+* height - Height above the surface of the earth
+* Calculates the acceleration do to gravity at a given height
+******************************************************************/
  inline float getGravity(float height)
 {
-   return GRAVITY * pow(EARTH_RADIUS / (EARTH_RADIUS + height),2);
+   return GRAVITY * (EARTH_RADIUS / (EARTH_RADIUS + height)) * (EARTH_RADIUS / (EARTH_RADIUS + height));
 }
 
+ /***************************************************************
+* getHeightAboveEarth(float x, float y)
+* x - Horizontal position
+* y - vertical position
+* Calculates the distance above the surface of the earth
+******************************************************************/
  inline float getHeightAboveEarth(float x, float y)
 {
-   return sqrt(pow(x,2) + pow(y,2)) - EARTH_RADIUS;
+   return sqrt((x * x) + (y * y)) - EARTH_RADIUS;
 }
 
+ /***************************************************************
+* getDirectionOfGravityPull(float x, float y)
+* x - Horizontal position
+* y - vertical position
+* Calculates the direction of the earth from the object
+******************************************************************/
  inline float getDirectionOfGravityPull(float x, float y)
 {
    return atan2(-x,-y);
 }
 
+ /***************************************************************
+* getHorizontalAccel(float angle, float accel)
+* angle - the angle of the moving object
+* accel - total acceleration
+* Calculates the horizontal component of acceleration, or accel
+* relative to X. This function can be used to find the horizontal
+* component of any angled vector.
+******************************************************************/
  inline float getHorizontalAccel(float angle, float accel)
 {
    return accel * sin(angle); // In radians
 }
 
+ /***************************************************************
+* getVerticalAccel(float angle, float accel)
+* angle - the angle of the moving object
+* accel - total acceleration
+* Calculates the Vertical component of acceleration, or accel
+* relative to Y. This function can be used to find the vertical
+* component of any angled vector.
+******************************************************************/
  inline float getVerticalAccel(float angle, float accel)
 {
    return accel * cos(angle); // In radians
 }
+
+ /***************************************************
+ * getChangedMagnitude(float so, float v, float t)
+ * so - the initial value that will be changed
+ * v  - the rate of change
+ * t  - time
+ * This formula is the same whether changing position 
+ * with velocity, or velocity with acceleration
+ ****************************************************/
 
  inline float getChangedMagnitude(float so, float v, float t) // also used for velocity components
 {
    return so + v * t;
 }
 
+ /***************************************************************
+* getDistance(float pos, float velocity, float time, float accel)
+* pos      - the initial value that will be changed
+* velocity - the speed of the object
+* time     - time in seconds
+* accel    - total acceleration
+* This formula calculates the new position of an object based on
+* it's current movement. It is meant to be used from second to
+* second
+******************************************************************/
  inline float getDistance(float pos, float velocity, float time, float accel)
 {
    return getChangedMagnitude(pos, velocity, time) + accel * .5 * time * time;
