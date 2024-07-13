4ed61d0c6eef1853067a512a976f7e48688c4b60
Taemour Djahanbani <73324097+DryTaegen@users.noreply.github.com>
2024-07-13 10:50:35 -0600

    Created base of fragment

---
+++ fragment.h
@@ -0,0 +1,15 @@
+/***********************************************************************
+ * Header File:
+ *    Fragment : The fragment class
+ * Author:
+ *    Taemour, Richard, Aria
+ * Summary:
+ *    Represents fragments within the simulation.
+ ************************************************************************/
+#pragma once
+#include "entity.h"
+class Fragment : public Entity
+{
+
+};
+
