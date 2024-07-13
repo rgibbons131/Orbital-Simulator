d913c21b82aa676d3daf9db48c6661a7b31da766
rgibbons131 <richiegibbons99@gmail.com>
2024-07-13 14:18:24 -0600

    Part offset added

--- part.h
+++ part.h
@@ -12,10 +12,20 @@
 class Part : public Satellite
 {
 public:
-   Part(void (ogstream::* drawFunction)(const Position& center,double rotation), const double& radius) : Satellite() { drawFunc = drawFunction;};
+   Part(void (ogstream::* drawFunction)(const Position& center,double rotation), const double& a, const double& radius) : Satellite() 
+   { 
+      drawFunc = drawFunction;
+      this->position.addPixelsX(getHorizontalAccel(a, 4));
+      this->position.addPixelsY(getVerticalAccel(a, 4));
+   };
    void (ogstream::* drawFunc)(const Position& center,double rotation);
 
-   Part(void (ogstream::* drawFunctionOffset)(const Position& center, double rotation, const Position& offset), const double& radius) : Satellite() { drawFuncOffset = drawFunctionOffset; };
+   Part(void (ogstream::* drawFunctionOffset)(const Position& center, double rotation, const Position& offset), const double& a, const double& radius) : Satellite()
+   { 
+      drawFuncOffset = drawFunctionOffset; 
+      this->position.addPixelsX(getHorizontalAccel(a, 4));
+      this->position.addPixelsY(getVerticalAccel(a, 4));
+   };
    void (ogstream::* drawFuncOffset)(const Position& center, double rotation, const Position& offset);
 
    void draw(ogstream* pgout)
